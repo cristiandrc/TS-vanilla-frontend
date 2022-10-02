6cb0470fd688338b838eb72de7d0c41b552a0ce2
Cristian Rojas <crisdarroca@hotmail.com>
2022-10-02 13:23:04 -0500

    add toastyfy-js

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import './style.css'
+import "toastify-js/src/toastify.css"
+
 import {Form} from './components/Form'
 import {TaskList} from './components/List'
 import { TaskInterface } from './models/task.model'
 import {Task} from './components/Task'
 import {v4} from 'uuid'
+import Toastify from 'toastify-js'
 
 
 let tasks:TaskInterface[] = []
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class='bg-slate-700 w-1/2 p-10 rounded-md ' >
+  <div class='bg-slate-700 w-1/2 p-10 rounded-md >
     <h1 class='text-2xl'>Tasks</h1>
     ${Form()}
   </div>
@@ -25,6 +28,15 @@ taskForm?.addEventListener('submit',(e)=>{
   const title = taskForm['title'] as unknown as HTMLInputElement
   const description = taskForm['description'] as unknown as HTMLTextAreaElement
 
+  if(!title.value || !description.value){
+    title.placeholder = 'Please type a Title'
+    description.placeholder = 'Please type a Description'
+
+    title.className = 'placeholder-red-600 bg-gray-800 p-2 rounded-md'
+    description.className = 'placeholder-red-600 bg-gray-800 p-2 rounded-md'
+    return
+  }
+
   const newTask = {
     title: title.value,
     description: description.value,
@@ -39,6 +51,10 @@ taskForm?.addEventListener('submit',(e)=>{
 
   title.value = ''
   description.value = ''
+  title.placeholder = 'Write a title'
+  description.placeholder = 'Write a Description'
+  title.className = ' bg-gray-800 p-2 rounded-md'
+  description.className = ' bg-gray-800 p-2 rounded-md'
 })
 
 
@@ -54,7 +70,13 @@ const deleteTask = (id:string) => {
 
   localStorage.setItem('task', JSON.stringify(tasks))
 
-  console.log(newTask)
+  Toastify({
+    text: "Task deleted",
+    className: "info",
+    style: {
+      background: "linear-gradient(to right, #d7816a , #bd4f6c)",
+    }
+  }).showToast();
 }
 
 const renderTask = (item:TaskInterface[]) => {
@@ -65,4 +87,12 @@ const renderTask = (item:TaskInterface[]) => {
 
 const addTask = (task:TaskInterface) => {
   taskList!.append(Task(task,deleteTask))
+  Toastify({
+    text: "Task added",
+    className: "warning",
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    }
+  }).showToast();
 }
+
